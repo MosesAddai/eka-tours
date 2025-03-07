a3a5a5740200bc46203c1d8679a402e3d46f64df
MosesAddai <maddaibaffour@gmail.com>
2025-03-07 22:38:59 +0000

    Structure Of Eka Tour project Done.
    -Used a vite Template.
    -Deleted unecessary codes.
    -Website title was chaged to "EKA Tours"
    -Different components needed was created. (Header, About, Footer, Contacts, Hero,Services etc.)
    -Different css files were created.
    -Began working on Header section and Hero sectoion.

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			<header className="header">
+				<div className="logo">EKA Tours</div>
+				<nav>
+					<ul className="nav-links">
+						<li>
+							<a href="#home">Home</a>
+						</li>
+						<li>
+							<a href="#about">About</a>
+						</li>
+						<li>
+							<a href="#services">Services</a>
+						</li>
+						<li>
+							<a href="#contact">Contact</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+	);
+};
+
+export default Header;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div>
+			<section className="hero">
+				<h1>Discover Amazing Places with EKA Tours</h1>
+				<p>Your gateway to unforgettable travel experiences.</p>
+				<button>Get Started</button>
+			</section>
+		</div>
+	);
+};
+
+export default Hero;
